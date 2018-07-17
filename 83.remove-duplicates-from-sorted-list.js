496061502ec3716a2cdf2b26e7e5466171f7db49
Molunerfinn <marksz@teamsz.xyz>
2018-07-17 17:11:22 +0800

    Finished: No.83

--- 83.remove-duplicates-from-sorted-list.js
+++ 83.remove-duplicates-from-sorted-list.js
@@ -38,5 +38,14 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-    
+  if (head === null || head.next === null) return head
+  let temp = head
+  while (temp.next !== null) {
+    if (temp.val === temp.next.val) {
+      temp.next = temp.next.next
+    } else {
+      temp = temp.next
+    }
+  }
+  return head
 };
